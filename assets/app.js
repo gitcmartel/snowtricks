6629f350811a8d90fef3f225f99230767ff6324e
gitcmartel <martel.c73@gmail.com>
2024-01-10 20:26:25 +0100

    Adding import statements for css files in app.js

---
+++ assets/app.js
@@ -0,0 +1,18 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * This file will be included onto the page via the importmap() Twig function,
+ * which should already be in your base.html.twig.
+ */
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles/style.css';
+import './styles/aos.css';
+import './styles/jquery-ui.css';
+import './styles/jquery.fancybox.min.css';
+import './styles/magnific-popup.css';
+import './styles/mediaelementplayer.css';
+import './styles/owl.carousel.min.css';
+import './styles/owl.theme.default.min.css';
+
+console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
