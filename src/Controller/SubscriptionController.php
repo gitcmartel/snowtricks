84768f383f71b08511e834a523be60a69673cec8
gitcmartel <martel.c73@gmail.com>
2024-01-13 19:55:12 +0100

    Adding password encryption

--- src/Controller/SubscriptionController.php
+++ src/Controller/SubscriptionController.php
@@ -2,17 +2,41 @@
 
 namespace App\Controller;
 
+use App\Form\RegistrationType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\User;
 
 class SubscriptionController extends AbstractController
 {
     #[Route('/subscription', name: 'app_subscription')]
-    public function index(): Response
+    public function index(Request $request, EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher): Response
     {
+        $user = new User();
+
+        $form = $this->createForm(RegistrationType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $hashedPassword = $passwordHasher->hashPassword(
+                $user, 
+                $user->getPassword()
+            );
+            $user->setPassword($hashedPassword);
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_home');
+        }
+
         return $this->render('subscription/index.html.twig', [
             'controller_name' => 'SubscriptionController',
+            'formUser' => $form->createView(),
         ]);
     }
 }
