813f3f18bb1568b3a690800afe58855d1570b591
gitcmartel <martel.c73@gmail.com>
2024-01-18 09:45:09 +0100

    Creation of ResetPassword form type

--- src/Form/ResetPasswordFormType.php
+++ src/Form/ResetPasswordFormType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ResetPasswordFormType extends AbstractType
@@ -12,6 +13,7 @@ class ResetPasswordFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
+            ->add('username', TextType::class)
             ->add('password', PasswordType::class)
         ;
     }
