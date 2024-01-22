8afa799dd21fedc1e5c897254f6d574f951a937f
gitcmartel <martel.c73@gmail.com>
2024-01-22 20:30:36 +0100

    Update HomeController.php
    Adding loadMoreTricks function.
    Adding const NUMBER_OF_TRICKS_PER_PAGE
    Using findTricksByPage instead of findAll in index function.

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,19 +4,40 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\TricksRepository;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class HomeController extends AbstractController
 {
+    private const NUMBER_OF_TRICKS_PER_PAGE = 15;
+
     #[Route('/', name: 'app_home')]
     public function index(TricksRepository $tricksRepository): Response
     {
-        $tricks = $tricksRepository->findAll();
+        $tricks = $tricksRepository->findTricksByPage(1, $this::NUMBER_OF_TRICKS_PER_PAGE);
 
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
             'tricksList' => $tricks
         ]);
+    }
+
+    #[Route('/home/{page}', name: 'app_home_load_more')]
+    public function loadMoreTricks(Request $request, TricksRepository $tricksRepository, SerializerInterface $serializer) 
+    {
+        if ($request->isXmlHttpRequest()) {
+            $page = $request->query->getInt('page', 1);
+
+            $tricks = $tricksRepository->findTricksByPage($page, 15);
+
+            $data = $serializer->serialize($tricks, 'json');
+
+            return new JsonResponse($data, 200, [], true);
+        }
+
+        throw $this->createNotFoundException('Erreur lors du chargement des tricks');
     }
 }
