46d0090b754dbf8a3834056c874c35e26c591e02
gitcmartel <martel.c73@gmail.com>
2024-01-23 20:07:06 +0100

    Update home.js
    Adding class TricksScroll and jquery.

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,4 +1,7 @@
 import '../app.js';
+import '../vendor/jquery/jquery.index.js';
 import ButtonScroll from './classes/ButtonScroll.js';
+import TricksScroll from './classes/TricksScroll.js';
 
 let buttonScroll = new ButtonScroll();
+let tricksScroll = new TricksScroll();
