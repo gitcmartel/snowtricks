f9a31a1991d1837cb7f5d86c870d94108f9584f8
gitcmartel <martel.c73@gmail.com>
2024-01-25 14:16:49 +0100

    Create TricksController.php

---
+++ src/Controller/TricksController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TricksController extends AbstractController
+{
+    #[Route('/tricks', name: 'app_tricks')]
+    public function index(): Response
+    {
+        return $this->render('tricks/index.html.twig', [
+            'controller_name' => 'TricksController',
+        ]);
+    }
+}
