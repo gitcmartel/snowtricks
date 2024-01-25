3dbe1433490d2d3becf4ecb344cea83bbde57583
gitcmartel <martel.c73@gmail.com>
2024-01-25 11:17:20 +0100

    Adjusting TricksScroll class

--- assets/js/classes/TricksScroll.js
+++ assets/js/classes/TricksScroll.js
@@ -1,10 +1,19 @@
 export default class TricksScroll
 {
+    page;
+    loading;
+    pageContainer;
+    spinner;
+    overlay;
+    stopLoadingTricks;
+
     constructor ()
     {
         this.page = 2;
         this.loading = false;
-        this.pageContainer = document.getElementById('pages');
+        this.pageContainer = document.getElementById("pages");
+        this.overlay = document.getElementById("spinner-container");
+        this.stopLoadingTricks = false;
 
         //Adding listeners
         window.addEventListener('scroll', this.windowScrollListener.bind(this));
@@ -12,23 +21,40 @@ export default class TricksScroll
     };
 
     windowScrollListener () {
-        if ((window.innerHeight + window.scrollY) >= (this.pageContainer.offsetTop + this.pageContainer.offsetHeight)) {
-            if (this.loading === false) {
+        if ((window.innerHeight + window.scrollY) >= (this.pageContainer.offsetTop + this.pageContainer.offsetHeight + 130)) {
+            if (this.loading === false && this.stopLoadingTricks === false) {
                 this.loading = true;
-                this.page++;
-                this.loadMoreTricks(this.page);
+                this.loadMoreTricks();
             }
         }
     };
 
-    loadMoreTricks(page) {
-        $.ajax({
-            url: '/home/' . page, 
-            method: 'GET', 
-            success: function(response) {
-                this.pageContainer.appendChild(response);
+    loadMoreTricks() {
+        this.overlay.classList.remove("d-none");
+
+        const xhr = new XMLHttpRequest();
+        xhr.open('GET', '/home/' + this.page, true);
+
+        xhr.onreadystatechange = () => {
+            if (xhr.readyState === 4) {
+                if (xhr.status === 200) {
+                    const html = xhr.responseText;
+                    if (html.includes("post-entry-1") === false) {
+                        this.stopLoadingTricks = true;
+                    } else {
+                        this.pageContainer.insertAdjacentHTML('beforeend', html);
+                        this.page++;
+                    }
+                } else {
+                    console.error('Erreur lors du chargement des tricks: ', xhr.status, xhr.statusText);
+                }
+                setTimeout(() => {
+                    this.overlay.classList.add("d-none");
+                }, 500);
                 this.loading = false;
             }
-        });
-    }
+        };
+
+        xhr.send();
+    };
 }
