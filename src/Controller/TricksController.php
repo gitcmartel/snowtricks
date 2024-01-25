12891881cd31e16a3ce1fd39acc8bd2b2ed007aa
gitcmartel <martel.c73@gmail.com>
2024-01-25 15:40:49 +0100

    Adding route trickId attribute

--- src/Controller/TricksController.php
+++ src/Controller/TricksController.php
@@ -5,14 +5,26 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\TricksRepository;
+use App\Repository\MediaRepository;
 
 class TricksController extends AbstractController
 {
-    #[Route('/tricks', name: 'app_tricks')]
-    public function index(): Response
+    #[Route('/tricks/{tricksId}', name: 'app_tricks')]
+    public function index($tricksId, TricksRepository $trickRepository, MediaRepository $mediaRepository): Response
     {
+        //Fetching the tricks and media data
+        $tricks = $trickRepository->findOneById($tricksId);
+        $media = $mediaRepository->findByTricks($tricksId);
+
+        if ($tricks == null) {
+            return $this->redirectToRoute('app_not_found');
+        }
+
         return $this->render('tricks/index.html.twig', [
             'controller_name' => 'TricksController',
+            'tricks' => $tricks, 
+            'media' => $media
         ]);
     }
 }
