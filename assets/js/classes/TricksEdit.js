7231cdba70e78cb92a4af496c470e888e14f43be
gitcmartel <martel.c73@gmail.com>
2024-01-28 12:39:10 +0100

    Create TricksEdit.js

---
+++ assets/js/classes/TricksEdit.js
@@ -0,0 +1,37 @@
+export default class TricksEdit {
+    deleteHeroImageButton;
+    InputHeroBackgroundImage;
+    hero;
+    functionNameToExecute;
+
+    constructor() {
+        this.modalYesButton = document.getElementById("modalYesButton");
+
+        this.deleteHeroImageButton = document.getElementById("deleteHeroImageButton");
+        this.hero = document.getElementById("hero");
+        this.InputHeroBackgroundImage = document.getElementById("InputHeroBackgroundImage");
+
+        // Adding listeners
+        this.modalYesButton.addEventListener("click", this.executeFunction.bind(this));
+        this.deleteHeroImageButton.addEventListener("click", this.setFunctionNameToExecute.bind(this));
+    }
+
+    setFunctionNameToExecute() {
+        this.functionNameToExecute = this.deleteHeroImageButton.getAttribute("data-function");
+    }
+
+    executeFunction() {
+        // Checks if the function name exists in the class or globally
+        if (typeof this[this.functionNameToExecute] === "function") {
+            // Execute the function
+            this[this.functionNameToExecute].call(this); 
+        } else {
+            console.error("La fonction " + this.functionNameToExecute + " n'existe pas ou n'est pas une fonction valide.");
+        }
+    }
+
+    deleteHeroImage() {
+        this.hero.style.backgroundImage = "";
+        this.InputHeroBackgroundImage.value = "";
+    }
+}
