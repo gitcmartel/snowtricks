f4509a81b525ebfccb8e18dad610de54c7a516f0
gitcmartel <martel.c73@gmail.com>
2024-01-28 18:47:30 +0100

    Adding modal property and hideModal method

--- assets/js/classes/ModalMessage.js
+++ assets/js/classes/ModalMessage.js
@@ -1,12 +1,18 @@
 export default class ModalMessage
 {
     modalMessage;
+    modal;
     constructor(message) {
         this.modalMessage = document.getElementById("modalMessage");
         this.setModalMessage(message);
+        this.modal = new bootstrap.Modal(document.getElementById('staticBackdrop'));
     }
 
     setModalMessage(message) {
         this.modalMessage.innerHTML = message;
+    }
+
+    hideModal() {
+        this.modal.hide();
     }
 }
