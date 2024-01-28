bcdb5287d1da7df316310276eebdebfdd74a5205
gitcmartel <martel.c73@gmail.com>
2024-01-28 10:07:49 +0100

    Create TricksFormType.php
    Creation of class TricksFormType

---
+++ src/Form/TricksFormType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Tricks;
+use App\Entity\TricksGroup;
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TricksFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('description')
+            ->add('image')
+            ->add('user', EntityType::class, [
+                'class' => User::class,
+'choice_label' => 'id',
+            ])
+            ->add('tricks_group', EntityType::class, [
+                'class' => TricksGroup::class,
+'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Tricks::class,
+        ]);
+    }
+}
