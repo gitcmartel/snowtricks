908f4e43c162d33d843879f8d451334df0d071ef
gitcmartel <martel.c73@gmail.com>
2024-02-01 17:07:36 +0100

    Create MediaFormType.php

---
+++ src/Form/MediaFormType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Media;
+use App\Entity\tricks;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class MediaFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('path')
+            ->add('type')
+            ->add('tricks', EntityType::class, [
+                'class' => tricks::class,
+'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Media::class,
+        ]);
+    }
+}
