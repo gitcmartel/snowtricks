4d93c4bbd71bd0817cfb00445fedff2a64f08052
gitcmartel <martel.c73@gmail.com>
2024-02-06 20:37:24 +0100

    Adding InputFileLit object

--- assets/js/tricks.js
+++ assets/js/tricks.js
@@ -1,5 +1,7 @@
 import TricksEdit from './classes/TricksEdit.js';
 import ModalMessage from './classes/ModalMessage.js';
+import InputFileList from './classes/InputFileList.js';
 
 
 let tricksEdit = new TricksEdit(new ModalMessage);
+let inputFileList = new InputFileList("tricks_form_medias", "fileList");
