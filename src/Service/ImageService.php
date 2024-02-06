cd893ade16a66053819fe0aed25216cd19517b4f
gitcmartel <martel.c73@gmail.com>
2024-02-06 20:37:56 +0100

    Create ImageService.php

---
+++ src/Service/ImageService.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Service;
+
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+
+class ImageService
+{
+    private $uploadDirectory;
+
+    public function __construct(ParameterBagInterface $uploadDirectory)
+    {
+        $this->uploadDirectory = $uploadDirectory;
+    }
+
+    /**
+     * Move an uploaded file
+     * @param UploadedFile $uploadedFile
+     * @return string New name of the uploaded file
+     */
+    public function moveUploadedFile (UploadedFile $uploadedFile) : string 
+    {
+        $originalFilename = pathinfo($uploadedFile->getClientOriginalName(), PATHINFO_FILENAME);
+        $newFilename = $originalFilename . '-' . uniqid() . '.' . $uploadedFile->guessExtension();
+
+        $uploadedFile->move(
+            $this->uploadDirectory,
+            $newFilename
+        );
+
+        return $newFilename;
+    }
+}
