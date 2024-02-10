bc81437ef531cf189a578eca25a4d31a38693e75
gitcmartel <martel.c73@gmail.com>
2024-02-10 20:02:34 +0100

    Update TricksEdit.js

--- assets/js/classes/TricksEdit.js
+++ assets/js/classes/TricksEdit.js
@@ -1,10 +1,11 @@
 export default class TricksEdit {
 
     deleteButtons;
+    editButtons;
 
     constructor(modalDialog) {
         this.deleteButtons = document.querySelectorAll('.delete-hero-image, .delete-media');
-        this.editButtons = document.getElementsByClassName('');
+        this.editButtons = document.querySelectorAll('.edit-hero-image, .edit-media');
 
         this.deleteButtons.forEach((button) => {
             button.addEventListener('click', () => {
@@ -26,21 +27,56 @@ export default class TricksEdit {
                 });
             });
         });
+
+        this.editButtons.forEach((button) => {
+            button.addEventListener('change', () => {
+                if (button.classList.contains('edit-hero-image')) {
+                    this.editImageTricks.bind(this)(event);
+                } else if (button.classList.contains('edit-media')) {
+                    this.editMedia.bind(this);
+                }
+            });
+        
+        });
+
+        document
+            .querySelectorAll('.add_item_link')
+            .forEach(btn => {
+                btn.addEventListener("click", this.addFormToCollection)
+        });
+
+        document
+            .querySelectorAll('ul.tags li')
+            .forEach((tag) => {
+                this.addTagFormDeleteLink(tag)
+        });
     }
 
     /**
      * Display the uploaded tricks image in the hero section
      * @param {*} event 
      */
-    displayImageTricks (event) {
+    editImageTricks (event) {
         // e.files contient un objet FileList
         const [picture] = event.target.files;
-    
+        const hero = document.getElementById('hero');
+
         // "picture" est un objet File
         if (picture) {
           // On change l"URL de l"image
-          this.hero.style.backgroundImage = `url(${URL.createObjectURL(picture)})`;
+          hero.style.backgroundImage = `url(${URL.createObjectURL(picture)})`;
+          //On repasse la valeur de l'input qui controle une eventuelle suppression a false
+          const isHeroImageDeleted = document.getElementById('tricks_form_isHeroImageDeleted');
+          isHeroImageDeleted.checked = false;
         }
+    }
+
+    /**
+     * 
+     */
+
+    editMedia () {
+
     }
 
     /**
@@ -48,6 +84,8 @@ export default class TricksEdit {
      */
     deleteHeroImage() {
         const hero = document.getElementById("hero");
+        const isHeroImageDeleted = document.getElementById('tricks_form_isHeroImageDeleted');
+        isHeroImageDeleted.checked = true;
         hero.style.backgroundImage = "";
     }
 
@@ -79,5 +117,39 @@ export default class TricksEdit {
      */
     showTricks() {
         this.mediaList.classList.remove("d-none");
+    }
+
+    addFormToCollection = (e) => {
+        const collectionHolder = document.querySelector('.' + e.currentTarget.dataset.collectionHolderClass);
+      
+        const item = document.createElement('li');
+      
+        item.innerHTML = collectionHolder
+          .dataset
+          .prototype
+          .replace(
+            /__name__/g,
+            collectionHolder.dataset.index
+          );
+      
+        collectionHolder.appendChild(item);
+      
+        collectionHolder.dataset.index++;
+
+        // add a delete link to the new form
+        this.addTagFormDeleteLink(item);
+      };
+
+      addTagFormDeleteLink = (item) => {
+        const removeFormButton = document.createElement('button');
+        removeFormButton.innerText = 'Delete this media';
+    
+        item.append(removeFormButton);
+    
+        removeFormButton.addEventListener('click', (e) => {
+            e.preventDefault();
+            // remove the li for the tag form
+            item.remove();
+        });
     }
 }
