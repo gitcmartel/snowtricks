b5fb2c01ec8cc323506c00738c76e66090f33f9b
gitcmartel <martel.c73@gmail.com>
2024-02-10 20:02:23 +0100

    Update TricksFormType.php

--- src/Form/TricksFormType.php
+++ src/Form/TricksFormType.php
@@ -2,6 +2,7 @@
 
 namespace App\Form;
 
+use App\Entity\Media;
 use App\Entity\Tricks;
 use App\Entity\TricksGroup;
 use App\Entity\User;
@@ -9,6 +10,9 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class TricksFormType extends AbstractType
@@ -16,22 +20,48 @@ class TricksFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
+        ->add('id', HiddenType::class)
             ->add('name')
             ->add('description')
-            ->add('image')
+            ->add('image', FileType::class, [
+                'multiple' => false, 
+                'mapped' => false, 
+                'required' => false, 
+                'attr' => [
+                    'accept' => '.png, .jpeg, .jpg, .svg',
+                    'maxsize' => '2M', 
+                    'class' => 'form-control edit-hero-image d-none'
+                ]
+            ])
             ->add('user', EntityType::class, [
                 'class' => User::class,
                 'choice_label' => 'id',
+                'attr' => [ 
+                    'class' => 'form-control d-none'
+                ]
             ])
             ->add('tricks_group', EntityType::class, [
                 'class' => TricksGroup::class,
                 'choice_label' => 'id',
             ])
-            ->add('medias', FileType::class, [
-                'label' => 'Ajouter des images ou vidéos',
-                'multiple' => true, 
-                'mapped' => false, 
-                'required' => false
+            ->add('medias', CollectionType::class, [
+                'entry_type' => MediaFormType::class, // Le type de formulaire pour chaque élément de la collection
+                'allow_add' => true, // Autorisez l'ajout dynamique d'éléments à la collection
+                'by_reference' => false, // Pour manipuler les objets directement, pas seulement les références
+                'allow_delete' => true, // Autorise la suppression d'éléments de la collection
+                'prototype' => true, // Activer le prototype pour les nouveaux éléments
+                'prototype_name' => '__media_name__', // Le nom du prototype
+                // Ajoutez d'autres options si nécessaire
+            ])
+            ->add('isHeroImageDeleted', CheckboxType::class, [
+                'label' => false,
+                'mapped' => false, // Ne pas mapper à une propriété de l'entité
+                'required' => false,
+                'data' => false,
+                'attr' => [
+                    'hidden' => true, // Rend le champ hidden
+                    'class' => 'is-hero-image-deleted' // Ajoute une classe pour le cibler avec JavaScript
+                ]
             ])
         ;
     }
