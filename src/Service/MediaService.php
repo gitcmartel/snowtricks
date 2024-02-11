a4f61b8730f66b27523c2542062bd82dc353c138
gitcmartel <martel.c73@gmail.com>
2024-02-11 18:25:10 +0100

    Renaming ImageService into MediaService

---
+++ src/Service/MediaService.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Service;
+
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+
+class MediaService
+{
+    private $uploadDirectory;
+
+    public function __construct(string $uploadDirectory)
+    {
+        $this->uploadDirectory = $uploadDirectory;
+    }
+
+    /**
+     * Move an uploaded file
+     * @param UploadedFile $uploadedFile
+     * @return string New name of the uploaded file
+     */
+    public function moveUploadedFile (UploadedFile $uploadedFile) : string 
+    {
+        $newFilename = uniqid() . '.' . $uploadedFile->guessExtension();
+
+        $uploadedFile->move(
+            $this->uploadDirectory,
+            $newFilename
+        );
+
+        return $newFilename;
+    }
+
+    /**
+     * Deletes a media if it's not the default image
+     * @param string $mediaPath
+     */
+    public function deleteMedia(string $mediaPath) {
+        $mediaName = basename($mediaPath);
+        if ($mediaName !== 'hero_1.jpg') {
+            unlink($this->uploadDirectory . '\\' . $mediaName);
+        }
+    }
+}
