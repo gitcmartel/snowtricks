711f274688121b3961969dd3200fb6db79e940be
gitcmartel <martel.c73@gmail.com>
2024-02-22 17:23:30 +0100

    Create message.js

---
+++ assets/js/message.js
@@ -0,0 +1,7 @@
+import MessagesList from './classes/MessagesList.js';
+import TricksImage from './classes/TricksImage.js';
+import SeeMediaButton from './classes/SeeMediaButton.js';
+
+let messageList = new MessagesList();
+let tricksImage = new TricksImage();
+let seeMediaButton = new SeeMediaButton();
