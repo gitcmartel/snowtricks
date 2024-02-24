85c2c97425a680b17ff8ae859b1ebd611fe1fd5b
gitcmartel <martel.c73@gmail.com>
2024-02-24 11:13:06 +0100

    Factoring code into ImageService class

--- src/Service/MediaService.php
+++ src/Service/MediaService.php
@@ -4,32 +4,9 @@ namespace App\Service;
 
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 
-class MediaService
+class MediaService extends ImageService
 {
-    private $uploadDirectory;
-
-    public function __construct(string $uploadDirectory)
-    {
-        $this->uploadDirectory = $uploadDirectory;
-    }
-
-    /**
-     * Move an uploaded file
-     * @param UploadedFile $uploadedFile
-     * @return string New name of the uploaded file
-     */
-    public function moveUploadedFile (UploadedFile $uploadedFile) : string 
-    {
-        $newFilename = uniqid() . '.' . $uploadedFile->guessExtension();
-
-        $uploadedFile->move(
-            $this->uploadDirectory,
-            $newFilename
-        );
-
-        return $newFilename;
-    }
-
+   
     /**
      * Deletes a media if it's not the default image
      * @param string $mediaPath
