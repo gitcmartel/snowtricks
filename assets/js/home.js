2f4c9c497df8658df340dc8e83a4b0f48bf02b6b
gitcmartel <martel.c73@gmail.com>
2024-02-28 20:48:40 +0100

    Update home.js

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,5 +1,7 @@
 import ButtonScroll from './classes/ButtonScroll.js';
 import TricksScroll from './classes/TricksScroll.js';
+import TricksDelete from './classes/TricksDelete.js';
 
 let buttonScroll = new ButtonScroll();
 let tricksScroll = new TricksScroll();
+let tricksDelete = new TricksDelete();
