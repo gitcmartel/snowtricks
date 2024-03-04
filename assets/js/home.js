9fbe0533c4d5eadf98461fcbd021d217ab35d7c9
gitcmartel <martel.c73@gmail.com>
2024-03-04 11:21:30 +0100

    Update home.js

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,7 +1,8 @@
 import ButtonScroll from './classes/ButtonScroll.js';
 import TricksScroll from './classes/TricksScroll.js';
 import TricksDelete from './classes/TricksDelete.js';
+import ModalMessage from './classes/ModalMessage.js';
 
 let buttonScroll = new ButtonScroll();
-let tricksScroll = new TricksScroll();
-let tricksDelete = new TricksDelete();
+let tricksDelete = new TricksDelete(new ModalMessage);
+let tricksScroll = new TricksScroll(tricksDelete);
