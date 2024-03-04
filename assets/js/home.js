81486640af7d3827f5326e890992dd5ace0a6dc7
gitcmartel <martel.c73@gmail.com>
2024-03-04 19:51:56 +0100

    Adding Toast.js class

--- assets/js/home.js
+++ assets/js/home.js
@@ -2,7 +2,9 @@ import ButtonScroll from './classes/ButtonScroll.js';
 import TricksScroll from './classes/TricksScroll.js';
 import TricksDelete from './classes/TricksDelete.js';
 import ModalMessage from './classes/ModalMessage.js';
+import Toast from './classes/Toast.js';
 
 let buttonScroll = new ButtonScroll();
 let tricksDelete = new TricksDelete(new ModalMessage);
 let tricksScroll = new TricksScroll(tricksDelete);
+let toast = new Toast();
