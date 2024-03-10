5f84c851eda77cb58836062cc9ce5cb1360519fe
gitcmartel <martel.c73@gmail.com>
2024-03-10 11:50:41 +0100

    Factoring toast messages into a service

--- src/EventSubscriber/IdentificationSubscriber.php
+++ src/EventSubscriber/IdentificationSubscriber.php
@@ -6,32 +6,25 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\Security\Http\Event\LoginSuccessEvent;
 use Symfony\Component\Security\Http\Event\LogoutEvent;
+use App\Service\ToastService;
 
 class IdentificationSubscriber implements EventSubscriberInterface
 {
-    private $requestStack;
+    private ToastService $toastService;
 
     public function __construct(RequestStack $requestStack)
     {
-        $this->requestStack = $requestStack;
+        $this->toastService = new ToastService($requestStack);
     }
 
     public function onLogout(LogoutEvent $event)
     {
-        $session = $this->requestStack->getSession();
-        $message = 'Disconnection success !';
-        
-        $session->set('toastMessage', $message);
-        $session->save();
+        $this->toastService->setMessage('Disconnection success !', 'success');
     }
 
     public function onLogin(LoginSuccessEvent $event)
     {
-        $session = $this->requestStack->getSession();
-        $message = 'Connection success !';
-        
-        $session->set('toastMessage', $message);
-        $session->save();
+        $this->toastService->setMessage('Connection success !', 'success');
     }
 
     public static function getSubscribedEvents()
