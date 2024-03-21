dcc248bc44f4ddbc6218c4e51198ae735d927527
gitcmartel <martel.c73@gmail.com>
2024-03-21 16:23:58 +0100

    Create Version20240321135109.php

---
+++ migrations/Version20240321135109.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240321135109 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE tricks ADD slug VARCHAR(255) NOT NULL');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_E1D902C1989D9B62 ON tricks (slug)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP INDEX UNIQ_E1D902C1989D9B62 ON tricks');
+        $this->addSql('ALTER TABLE tricks DROP slug');
+    }
+}
